58fbd509a8f9b3bdb3371a6ff8ad2a0d24975454
MikhailoDmytriuk <cornijangel2077@gmail.com>
2023-10-28 18:30:15 +0300

    first commit

---
+++ notes_main.py
@@ -0,0 +1,30 @@
+#почни тут створювати додаток з розумними замітка
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("Розумні замітки")
+window.resize(900, 600)
+
+
+
+qh_def = QHBoxLayout()
+qh1_def = QHBoxLayout()
+qv1_def = QVBoxLayout()
+qv2_def = QVBoxLayout()
+qv3_def = QVBoxLayout()
+qv4_def = QVBoxLayout()
+qv5_def = QVBoxLayout()
+qv6_def = QVBoxLayout()
+qv7_def = QVBoxLayout()
+qv8_def = QVBoxLayout()
+
+
+
+
+
+window.show()
+app.exec_()
+
+
